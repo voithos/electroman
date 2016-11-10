81c458cc1172c56437e1b615a81447def086f8b9
Zaven Muradyan <zaven@skepsi.me>
2016-11-09 22:47:47 -0800

    Initial commit.

---
+++ TODO.md
@@ -0,0 +1,6 @@
+TODO
+====
+
+- setup basic platforming / run-and-jump with minimal assets
+- begin adding attacks, health, enemies
+- go from there
---
+++ accum.gd
@@ -0,0 +1,17 @@
+
+extends Label
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+var accum = 0
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process(true)
+
+func _process(delta):
+	accum += delta
+	set_text(str(accum))
